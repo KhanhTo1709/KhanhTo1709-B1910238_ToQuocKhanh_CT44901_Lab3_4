1545fa90157788e4277bc7900918ff2a1232b542
Khanh To <toquockhanh2001@gmail.com>
2023-04-05 00:35:06 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -12,12 +12,14 @@ const routes = [
         component: () => import("@/views/ContactAdd.vue"),
         props: true // Truyền các biến trong $route.params vào làm props
     },
+    
     {
-        path: "/contacts/:id",
+        path: "/contacts/:id" ,
         name: "contact.edit",
         component: () => import("@/views/ContactEdit.vue"),
         props: true // Truyền các biến trong $route.params vào làm props
     },
+    
     {
         path: "/:pathMatch(.*)*",
         name: "notfound!",
@@ -29,4 +31,4 @@ const router = createRouter({
     routes,
 });
 
-export default router;
+export default router;git add -u
